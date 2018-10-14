e51013264e5ca132e96f6cf2c0ecd4d566a73e4d
jopor <elop@gazeta.pl>
2018-10-14 22:31:09 +0200

    Add TODO tasks

--- POMTesting/Helpers/InitClass.cs
+++ POMTesting/Helpers/InitClass.cs
@@ -28,8 +28,10 @@ namespace Store.PageObjects
         {
             if (TestContext.CurrentContext.Result.Outcome.Status.Equals(TestStatus.Failed))
             {
+                // TODO: 141018 - Add screenshot for failed tests
                 log.Error($"{TestContext.CurrentContext.Test.Name} test failed");
             }
+
             log.Info($"> {TestContext.CurrentContext.Test.Name} test successfully finished");
 
             driver.Dispose();
@@ -39,7 +41,8 @@ namespace Store.PageObjects
         {
             switch (browserType)
             {
-                case "Firefox":
+                // TODO: 141018 - Fix broken tests using Firefox browser
+                case "Firefox": 
                     driver = new FirefoxDriver();
                     break;
                 case "Chrome":
